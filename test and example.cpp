431a2cde2dc5f20fb82f075e759e9044321ffb6f
Hasib27-gg <mdabdulalhasib@gmail.com>
2025-12-14 15:55:26 +0600

    Update test and example.cpp

--- test and example.cpp
+++ test and example.cpp
@@ -7,31 +7,34 @@ int main()
 	    The code down is a sample code for prime checking.
 		Check the API Reference for syntax.
 	*/
-	std::string code = R"(
-       #main{
-          @new_i32 : (n , 7);
-          @execute : (isPrime);
-          @print   : ("Is N a prime number?: " , $bool);
-       }
-       #isPrime{
-          @import : (main , n);
-          @new_i32 : (count , 0);
-          @new_i32 : (it , 0);
-          @new_i32 : (mod_res, 0);
-          @new_bool : (bool , false);
-          @loop     : (1 , $n , 1) ~ it;
-          @start    : (it);
-              @mod  : ($n , $it) ~ mod_res;
-              @isEqual : ($mod_res , 0) ~ bool;
-              <$bool> @add : ($count , 1) ~ count; 
-          @end      : (it);
-          @isEqual : ($count , 2) ~ bool;
-          @export  : (main ,bool);
-          @delete_var : (count);
-          @delete_var : (it);
-          @delete_var : (mod_res);
-       }
-    )";
+std::string code = R"(
+   #main{
+      @new_str : (buffer , "");
+      @input   : ("Enter your number here: ") ~ buffer;
+      @new_i32 : (n , $buffer);
+      @execute : (isPrime);
+      @print   : ("Is " , $n ," a prime number?: " , $bool);
+   }
+   #isPrime{
+      @import : (main , n);
+      @new_i32 : (count , 0);
+      @new_i32 : (it , 0);
+      @new_i32 : (mod_res, 0);
+      @new_bool : (bool , false);
+      @loop     : (1 , $n , 1) ~ it;
+      @start    : (it);
+          @mod  : ($n , $it) ~ mod_res;
+          @isEqual : ($mod_res , 0) ~ bool;
+          <$bool> @add : ($count , 1) ~ count; 
+      @end      : (it);
+      @isEqual : ($count , 2) ~ bool;
+      @export  : (main ,bool);
+      @export   : (main , n);
+      @delete_var : (count);
+      @delete_var : (it);
+      @delete_var : (mod_res);
+   }
+)";
      /*
 	    First , we need to compile the code from raw string instructions to actual VM readable instruction.
 	 */
@@ -44,5 +47,6 @@ int main()
 	wrapper.execute();
 	return 0;
 }
+
 
 
